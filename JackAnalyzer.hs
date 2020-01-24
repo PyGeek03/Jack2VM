785c69b9c83a42d4ff5eb2c1f6959aedcc3497dd
PyGeek03 <PyGeek03@users.noreply.github.com>
2020-01-25 03:20:34 +0700

    Finish tokenizer

--- JackAnalyzer.hs
+++ JackAnalyzer.hs
@@ -1,11 +1,28 @@
 module Main where
 import System.Environment
-import Tokenizer (tokenize)
+import Data.List (isSuffixOf)
+import System.FilePath (dropExtension, (</>))
+import System.Directory (getDirectoryContents)
+import Tokenizer (tokenize, Token)
 
-main :: IO ()
+compileJack :: FilePath -> IO ()
+compileJack jackFile = do
+    contents <- readFile jackFile
+    let tXmlFile = (dropExtension jackFile) ++ "T.xml"
+        xmlFile = (dropExtension jackFile) ++ ".xml"
+        tokens = tokenize contents
+    writeFile tXmlFile $ show tokens
+
+compileDir :: FilePath -> IO [()]
+compileDir dirPath = do
+    allFilenames <- getDirectoryContents dirPath
+    let allFiles = map (dirPath </>) allFilenames
+        jackFiles = filter (\x -> ".jack" `isSuffixOf` x) allFiles
+    mapM compileJack jackFiles
+
+main :: IO [()]
 main = do
-    (input:[]) <- getArgs
-    contents <- readFile input
-    -- (putStr . unlines . tokenize) contents
-    (print . tokenize) contents
-    -- print contents
+    (inputPath:[]) <- getArgs
+    if (dropExtension inputPath) == inputPath  -- input is a directory
+        then compileDir inputPath
+        else mapM compileJack [inputPath]
--- Tokenizer.hs
+++ Tokenizer.hs
@@ -1,19 +1,89 @@
 module Tokenizer
 (tokenize
+, Token
 ) where
-import System.IO
+--import System.IO
 import Data.List
+import Text.Read (readMaybe)
+import Data.Maybe (isJust, fromJust)
 
-splitWords = map words
+-- Data type for each token
+data Token = Keyword String | Symbol Char | IntConst Int | StrConst String | Identifier String
 
-removeComments :: [[String]] -> [[String]]
-removeComments = 
-    let findCommentStart singleLine = 
-            case findIndex (\w -> w `elem` ["//", "/*", "/**", "*", "*/"]) singleLine of
-                Just i  -> i
-                Nothing -> length singleLine
-        removeComment singleLine = take (findCommentStart singleLine) singleLine
-    in map removeComment
+showToken :: Token -> String
+showToken (Keyword k) = "<keyword> " ++ k ++ " </keyword>"
+showToken (Symbol s) = case s of
+    '<' -> "<symbol> &lt; </symbol>"
+    '>' -> "<symbol> &gt; </symbol>"
+    '&' -> "<symbol> &amp; </symbol>"
+    _   -> "<symbol> " ++ [s] ++ " </symbol>"
+showToken (IntConst ic) = "<integerConstant> " ++ show ic ++ " </integerConstant>"
+showToken (StrConst sc) = "<stringConstant> " ++ sc ++ " </stringConstant>"
+showToken (Identifier i) = "<identifier> " ++ i ++ " </identifier>"
 
-tokenize :: String -> [String]
-tokenize = mconcat . removeComments . splitWords . lines
+instance Show Token where
+    show = showToken
+
+-- Data type for all tokens in a Jack file
+data Tokens = Tokens [Token]
+
+showTokens :: Tokens -> String
+showTokens (Tokens ts) = "<tokens>\n" ++ (unlines $ map show ts) ++ "</tokens>\n"
+
+instance Show Tokens where
+    show = showTokens
+
+-- Lexical elements
+keywords =  [ "class", "constructor", "function", "method", "field"
+            , "static", "var", "int", "char", "boolean", "void"
+            , "true", "false", "null", "this", "let", "do", "if"
+            , "else", "while", "return" ]
+symbols = "{}()[].,;+-*/&|<>=~"
+whitespace = " \r\n\t"
+
+readInt :: String -> Maybe Int
+readInt token = readMaybe token :: Maybe Int
+
+classify :: String -> Token
+classify token = case readInt token of
+    Just i -> IntConst i
+    Nothing
+        | "\"" `isPrefixOf` token -> StrConst $ init $ tail token
+        | token `elem` keywords   -> Keyword token
+        | otherwise               -> Identifier token    
+
+makeTokens :: String -> String -> [Token] -> Tokens
+makeTokens acc source reversedTokenList = case (acc, source) of
+    -- Base case: source is empty -> return
+    (_, [])        -> Tokens $ reverse reversedTokenList
+    -- Deal with '/'
+    ("/", x:xs) ->
+        case x of
+            '/' -> makeTokens "//" xs reversedTokenList  -- starts new comment
+            '*' -> makeTokens "/*" xs reversedTokenList  -- starts new multiline comment
+            _   -> makeTokens "" xs $ (Symbol '/'):reversedTokenList  -- new '/' symbol
+    ("//", x:xs)
+        | x `elem` "\r\n" -> makeTokens "" xs reversedTokenList  -- end of line -> end of comment
+        | otherwise       -> makeTokens "//" xs reversedTokenList  -- still in comment
+    ("/*", x:y:xs) ->
+        case [x, y] of
+            "*/" -> makeTokens "" xs reversedTokenList  -- end of multiline comment
+            _    -> makeTokens "/*" (y:xs) reversedTokenList  -- still in multiline comment
+    -- Build actual tokens
+    ('"':as, x:xs) ->
+        case x of
+            '"' -> makeTokens "" xs $ (StrConst $ tail acc):reversedTokenList  -- completed stringConst
+            _   -> makeTokens (acc ++ [x]) xs reversedTokenList  -- still in stringConst
+    ("", x:xs)
+        | x `elem` whitespace -> makeTokens "" xs reversedTokenList  -- skip whitespace
+        | x == '/'            -> makeTokens "/" xs reversedTokenList  -- '/' could either be a standalone symbol or the beginning of a comment
+        | x `elem` symbols    -> makeTokens "" xs $ (Symbol x):reversedTokenList  -- new symbol
+        | otherwise           -> makeTokens [x] xs reversedTokenList  -- starts new token
+    (_, x:xs)
+        | x `elem` whitespace -> makeTokens "" xs $ (classify acc):reversedTokenList  -- completed token
+        | x == '/'            -> makeTokens "/" xs $ (classify acc):reversedTokenList  -- '/' could either be a standalone symbol or the beginning of a comment
+        | x `elem` symbols    -> makeTokens "" xs $ (Symbol x):(classify acc):reversedTokenList  -- completed token & new symbol
+        | otherwise           -> makeTokens (acc ++ [x]) xs reversedTokenList  -- continues current token
+
+tokenize :: String -> Tokens
+tokenize source = makeTokens "" source []

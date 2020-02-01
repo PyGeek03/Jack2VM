6bcbec1a92be0f71a34842d1b134b832afdc82e4
PyGeek03 <PyGeek03@users.noreply.github.com>
2020-02-01 14:57:08 +0700

    Separate data structure and algorithm in 2 files

--- Tokenizer.hs
+++ Tokenizer.hs
@@ -1,89 +1,53 @@
 module Tokenizer
-(tokenize
-, Token
-) where
---import System.IO
-import Data.List
+(tokenize) where
+import Data.List (isPrefixOf)
 import Text.Read (readMaybe)
-import Data.Maybe (isJust, fromJust)
 
--- Data type for each token
-data Token = Keyword String | Symbol Char | IntConst Int | StrConst String | Identifier String
+import Tokens
 
-showToken :: Token -> String
-showToken (Keyword k) = "<keyword> " ++ k ++ " </keyword>"
-showToken (Symbol s) = case s of
-    '<' -> "<symbol> &lt; </symbol>"
-    '>' -> "<symbol> &gt; </symbol>"
-    '&' -> "<symbol> &amp; </symbol>"
-    _   -> "<symbol> " ++ [s] ++ " </symbol>"
-showToken (IntConst ic) = "<integerConstant> " ++ show ic ++ " </integerConstant>"
-showToken (StrConst sc) = "<stringConstant> " ++ sc ++ " </stringConstant>"
-showToken (Identifier i) = "<identifier> " ++ i ++ " </identifier>"
 
-instance Show Token where
-    show = showToken
-
--- Data type for all tokens in a Jack file
-data Tokens = Tokens [Token]
-
-showTokens :: Tokens -> String
-showTokens (Tokens ts) = "<tokens>\n" ++ (unlines $ map show ts) ++ "</tokens>\n"
-
-instance Show Tokens where
-    show = showTokens
-
--- Lexical elements
-keywords =  [ "class", "constructor", "function", "method", "field"
-            , "static", "var", "int", "char", "boolean", "void"
-            , "true", "false", "null", "this", "let", "do", "if"
-            , "else", "while", "return" ]
-symbols = "{}()[].,;+-*/&|<>=~"
+whitespace :: [Char]
 whitespace = " \r\n\t"
 
-readInt :: String -> Maybe Int
-readInt token = readMaybe token :: Maybe Int
 
 classify :: String -> Token
 classify token = case readInt token of
-    Just i -> IntConst i
+    Just i -> IC i
     Nothing
-        | "\"" `isPrefixOf` token -> StrConst $ init $ tail token
-        | token `elem` keywords   -> Keyword token
-        | otherwise               -> Identifier token    
+        | "\"" `isPrefixOf` token -> SC $ init $ tail token
+        | token `elem` keywords   -> KW token
+        | otherwise               -> ID token
+  where
+    readInt t = readMaybe t :: Maybe Int
 
-makeTokens :: String -> String -> [Token] -> Tokens
-makeTokens acc source reversedTokenList = case (acc, source) of
+makeTokens :: String -> [Token] -> String -> Tokens
+makeTokens acc reversedTokenList source = case (acc, source) of
     -- Base case: source is empty -> return
-    (_, [])        -> Tokens $ reverse reversedTokenList
-    -- Deal with '/'
-    ("/", x:xs) ->
-        case x of
-            '/' -> makeTokens "//" xs reversedTokenList  -- starts new comment
-            '*' -> makeTokens "/*" xs reversedTokenList  -- starts new multiline comment
-            _   -> makeTokens "" xs $ (Symbol '/'):reversedTokenList  -- new '/' symbol
+    (_,       []) -> Tokens $ reverse reversedTokenList
+    -- '/' is either a standalone symbol or the beginning of a comment
+    ("",  '/':xs) -> makeTokens "/"  reversedTokenList xs
+    ("/", '*':xs) -> makeTokens "/*" reversedTokenList xs  -- starts new multiline comment
+    ("/", '/':xs) -> makeTokens "//" reversedTokenList xs  -- starts new comment
+    ("/",  _ :xs) -> makeTokens ""   (SYM '/' : reversedTokenList) xs  -- new '/' symbol
+    -- Deal with multiline comments
+    ("/*", '*':'/':xs) -> makeTokens ""   reversedTokenList xs  -- end of multiline comment
+    ("/*",  _ : y :xs) -> makeTokens "/*" reversedTokenList $ y:xs -- still in multiline comment
     ("//", x:xs)
-        | x `elem` "\r\n" -> makeTokens "" xs reversedTokenList  -- end of line -> end of comment
-        | otherwise       -> makeTokens "//" xs reversedTokenList  -- still in comment
-    ("/*", x:y:xs) ->
-        case [x, y] of
-            "*/" -> makeTokens "" xs reversedTokenList  -- end of multiline comment
-            _    -> makeTokens "/*" (y:xs) reversedTokenList  -- still in multiline comment
-    -- Build actual tokens
-    ('"':as, x:xs) ->
-        case x of
-            '"' -> makeTokens "" xs $ (StrConst $ tail acc):reversedTokenList  -- completed stringConst
-            _   -> makeTokens (acc ++ [x]) xs reversedTokenList  -- still in stringConst
+        | x `elem` "\r\n" -> makeTokens ""   reversedTokenList xs  -- end of line -> end of comment
+        | otherwise       -> makeTokens "//" reversedTokenList xs  -- still in comment
+    -- Build string constants
+    ('"':_, '"':xs) -> makeTokens "" ((SC $ tail acc):reversedTokenList) xs  -- completed stringConst
+    ('"':_,  x :xs) -> makeTokens (acc ++ [x]) reversedTokenList xs  -- still in stringConst
+    -- Build other tokens
     ("", x:xs)
-        | x `elem` whitespace -> makeTokens "" xs reversedTokenList  -- skip whitespace
-        | x == '/'            -> makeTokens "/" xs reversedTokenList  -- '/' could either be a standalone symbol or the beginning of a comment
-        | x `elem` symbols    -> makeTokens "" xs $ (Symbol x):reversedTokenList  -- new symbol
-        | otherwise           -> makeTokens [x] xs reversedTokenList  -- starts new token
-    (_, x:xs)
-        | x `elem` whitespace -> makeTokens "" xs $ (classify acc):reversedTokenList  -- completed token
-        | x == '/'            -> makeTokens "/" xs $ (classify acc):reversedTokenList  -- '/' could either be a standalone symbol or the beginning of a comment
-        | x `elem` symbols    -> makeTokens "" xs $ (Symbol x):(classify acc):reversedTokenList  -- completed token & new symbol
-        | otherwise           -> makeTokens (acc ++ [x]) xs reversedTokenList  -- continues current token
+        | x `elem` whitespace -> makeTokens ""  reversedTokenList xs  -- skip whitespace
+        | x `elem` symbols    -> makeTokens ""  (SYM x:reversedTokenList) xs  -- new symbol
+        | otherwise           -> makeTokens [x] reversedTokenList xs  -- starts new token
+    (_, '/':xs) -> makeTokens "/"  (classify acc : reversedTokenList) xs
+    (_,  x :xs)
+        | x `elem` whitespace -> makeTokens "" (classify acc : reversedTokenList) xs  -- completed token
+        | x `elem` symbols    -> makeTokens "" (SYM x : classify acc : reversedTokenList) xs  -- completed token & new symbol
+        | otherwise           -> makeTokens (acc ++ [x]) reversedTokenList xs -- continues current token
 
 tokenize :: String -> Tokens
-tokenize source = makeTokens "" source []
+tokenize = makeTokens "" []
---
+++ Tokens.hs
@@ -0,0 +1,34 @@
+module Tokens where 
+import Data.List (unlines)
+
+-- Lexical elements
+keywords :: [String]
+keywords =  [ "class", "constructor", "function", "method", "field"
+            , "static", "var", "int", "char", "boolean", "void"
+            , "true", "false", "null", "this", "let", "do", "if"
+            , "else", "while", "return" ]
+
+symbols :: [Char]
+symbols = "{}()[].,;+-*/&|<>=~"
+
+-- Data type to "wrap" all tokens in one type
+data Token 
+    = KW  !String
+    | SYM !Char
+    | IC  !Int
+    | SC  !String
+    | ID  !String
+instance Show Token where
+    show (KW k) = "<keyword> " ++ k ++ " </keyword>"
+    show (SYM s) = case s of
+        '<' -> "<symbol> &lt; </symbol>"
+        '>' -> "<symbol> &gt; </symbol>"
+        '&' -> "<symbol> &amp; </symbol>"
+        _   -> "<symbol> " ++ [s] ++ " </symbol>"
+    show (IC ic) = "<integerConstant> " ++ shows ic " </integerConstant>"
+    show (SC sc) = "<stringConstant> " ++ sc ++ " </stringConstant>"
+    show (ID i) = "<identifier> " ++ i ++ " </identifier>"
+
+newtype Tokens = Tokens [Token]
+instance Show Tokens where
+    show (Tokens ts) = "<tokens>\n" ++ (unlines $ map show ts) ++ "</tokens>\n"
